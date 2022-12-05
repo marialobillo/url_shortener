835e10efdefe73742d8588a9e7d51a6fba819ff6
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2022-12-05 20:40:09 +0100

    feat: adding setup url

--- src/functions/setUrl/index.ts
+++ src/functions/setUrl/index.ts
@@ -1,12 +1,31 @@
-import { FormatJSONResponse } from '@libs/apiGateway';
+import { formatJSONResponse } from '@libs/apiGateway';
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { v4 as uuid } from 'uuid';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
+    const body = JSON.parse(event.body);
+    const tableName = process.env.urlTable;
+    const baseUrl = process.env.baseUrl;
+
+    const originalUrl = body.url;
+
+    const code = uuid().slice(0,8);
+
+    const shortUrl = `${baseUrl}/${code}`;
+
+    const data = {
+      id: code,
+      shortUrl,
+      originalUrl,
+    };
+    await dynamo.write(data, tableName)
+
+    return formatJSONResponse({data: { shortUrl, originalUrl}});
 
   } catch (error) {
     console.log('error', error)
-    return FormatJSONResponse({
+    return formatJSONResponse({
       statusCode: 502,
       data: {
         message: error.message,
