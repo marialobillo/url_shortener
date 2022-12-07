6cac746acf2e341af085bcdeebaeba4a1245b3e1
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2022-12-07 18:29:29 +0100

    feat: adding getUrl to dynamo custom library

--- src/libs/dynamo.ts
+++ src/libs/dynamo.ts
@@ -1,7 +1,7 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { PutCommand, PutCommandInput } from '@aws-sdk/lib-dynamodb'
+import { PutCommand, PutCommandInput, GetCommand, GetCommandInput } from '@aws-sdk/lib-dynamodb'
 
-const dynamodbClient = new DynamoDBClient({});
+const dynamodbClient  = new DynamoDBClient({});
 
 
 export const dynamo = {
@@ -17,4 +17,16 @@ export const dynamo = {
 
     return data
   },
+  get: async (id: string, tableName: string) => {
+    const params: GetCommandInput = {
+      TableName: tableName,
+      Key: {
+        id,
+      }
+    }
+    const command = new GetCommand(params)
+    const response = await dynamodbClient.send(command);
+
+    return response.Item;
+  } 
 };
